94877692538e255ab5e566f78cd807a456330d9b
sofiamatulis <sosomatulis@hotmail.com>
2016-11-30 19:46:23 -0500

    testing react

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// or no need to write function
+// const App = function () {
+  const App =  () => {
+
+
+  return<h1>Hello World from React</h1>;
+};
+
+ReactDOM.render(<App />, document.querySelector('.container'));
+
+// react test
+
+// npm start
